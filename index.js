d6dd4fff44f56bc97575df19f58a55d0d4bb82ca
Akeem <AkeemS11788@gmail.com>
2021-11-02 11:22:06 -0400

    commit tree 1239c01ed36cd93637686943d87c51bd7fb5f7ff

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Your code here
+function saturdayFun(activity = "roller-skate") {
+   return `This Saturday, I want to ${activity}!`
+};
+
+const mondayWork = function (mondayActivity = "go to the office") {
+    return `This Monday, I will ${mondayActivity}.`
+};
+
+// function wrapAdjective() {
+//     return function theChild(loneParameter = "special") {
+//                 return `You are ${loneParameter}!`
+//         };
+// };
+
+function wrapAdjective(flare = "*") {
+    return function(adj) {
+        return `You are ${flare}${adj}${flare}!`
+    };
+}
